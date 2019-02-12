42576262bb9b2254691a3220afe5d561b3d82e36
mario-5 <mario@iotarch.com.ar>
2019-02-12 15:38:12 -0300

    Dia VI

---
+++ src/org/cadenhead/ecommerce/Storefront.java
@@ -0,0 +1,26 @@
+package org.cadenhead.ecommerce;
+
+import java.util.*;
+
+public class Storefront {
+    private LinkedList catalog = new LinkedList();
+
+    public void addItem(String id, String name, String price,
+        String quant) {
+
+        Item it = new Item(id, name, price, quant);
+        catalog.add(it);
+    }
+
+    public Item getItem(int i) {
+        return (Item) catalog.get(i);
+    }
+
+    public int getSize() {
+        return catalog.size();
+    }
+
+    public void sort() {
+        Collections.sort(catalog);
+    }
+}
