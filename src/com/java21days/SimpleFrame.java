df6f2dc68ff03e96020a8fc87628774d46e4e364
mario-5 <mario@iotarch.com.ar>
2019-02-15 18:28:44 -0300

    Dia IX

---
+++ src/com/java21days/SimpleFrame.java
@@ -0,0 +1,28 @@
+package com.java21days;
+
+import javax.swing.*;
+
+public class SimpleFrame extends JFrame {
+    public SimpleFrame() {
+        super("Frame Title");
+        setSize(300, 100);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLookAndFeel();
+        setVisible(true);
+    }
+
+    private static void setLookAndFeel() {
+        try {
+            UIManager.setLookAndFeel(
+                "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel"
+            );
+        } catch (Exception exc) {
+            // ignore error
+        }
+    }
+
+    public static void main(String[] arguments) {
+        setLookAndFeel();
+        SimpleFrame sf = new SimpleFrame();
+    }
+}
