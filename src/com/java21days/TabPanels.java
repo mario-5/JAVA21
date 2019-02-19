539d897c8f31292d7d04353a7cb42da62da577aa
mario-5 <mario@iotarch.com.ar>
2019-02-19 15:30:56 -0300

    Day X

---
+++ src/com/java21days/TabPanels.java
@@ -0,0 +1,43 @@
+package com.java21days;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class TabPanels extends JFrame {
+
+    public TabPanels() {
+        super("Tabbed Panes");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLookAndFeel();
+        setSize(480, 218);
+        JPanel mainSettings = new JPanel();
+        JPanel advancedSettings = new JPanel();
+        JPanel privacySettings = new JPanel();
+        JPanel emailSettings = new JPanel();
+        JPanel securitySettings = new JPanel();
+        JTabbedPane tabs = new JTabbedPane();
+        tabs.addTab("Main", mainSettings);
+        tabs.addTab("Advanced", advancedSettings);
+        tabs.addTab("Privacy", privacySettings);
+        tabs.addTab("E-mail", emailSettings);
+        tabs.addTab("Security", securitySettings);
+        add(tabs);
+        setVisible(true);
+    }
+
+    private void setLookAndFeel() {
+        try {
+            UIManager.setLookAndFeel(
+                "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel"
+            );
+            SwingUtilities.updateComponentTreeUI(this);
+        } catch (Exception e) {
+            System.err.println("Couldn't use the system "
+                + "look and feel: " + e);
+        }
+    }
+
+    public static void main(String[] arguments) {
+        TabPanels frame = new TabPanels();
+    }
+}
